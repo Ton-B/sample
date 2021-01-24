94cc755e319efe760e1f3410fd15316b84966d5e
masaki <mw8931601081107@gmail.com>
2021-01-24 19:53:38 +0900

    third commit

--- todo/views.py
+++ todo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def samplefunc(request):
-    return render(request, 'sample.html',{})
+    return render(request, 'sample.html',{"sample":"1"})
